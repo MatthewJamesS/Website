e886522932227dd3eae166e828860df473c3ffeb
MatthewJamesS <matthewsmith.mjs@gmail.com>
2019-08-15 16:50:34 +1200

    playing_with_view

---
+++ main.js
@@ -0,0 +1,8 @@
+var app = new VTTCue(
+    {
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: '/Users/matthewsmith/Documents/workspace/website/vmSocks-green-onWhite.jpg'
+    }
+})
