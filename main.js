14e5e6fd985944a1978f8f30cdca5190a01c7452
MatthewJamesS <matthewsmith.mjs@gmail.com>
2019-08-18 23:52:48 +1200

    fail_at_adding_colourchange_button

--- main.js
+++ main.js
@@ -4,6 +4,27 @@ var app = new Vue({
         product: 'Socks',
         image: '/Users/matthewsmith/Documents/workspace/website/vmSocks-green-onWhite.jpg',
         inventory: true,
-        details: ["80% cotton", "20% polyester", "Gender-neutral"]
+        variants: [ 
+            {
+                variantId: 2234,
+                variantColour: "green",
+                variantImage: '/Users/matthewsmith/Documents/workspace/website/vmSocks-green-onWhite.jpg'
+            },
+            {
+                variantId: 2235,
+                variantColour: "blue",
+                variantImage: '/Users/matthewsmith/Documents/workspace/website/vmSocks-blue-onWhite.jpg'
+            }
+        ],
+        cart: 0
+    },
+    methods:{
+        addToCart: function() {
+            this.cart += 1
+        },
+        updateProduct: function (variantImage){
+            this.image = variantImage
+        }
+
     }
 })
